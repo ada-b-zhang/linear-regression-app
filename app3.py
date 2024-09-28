983a8816eb10889d0cf78658c2510bc830a14025
ada_zhang <abz200026@gmail.com>
2024-09-28 13:10:28 -0700

    cleaned app3.py

--- app3.py
+++ app3.py
@@ -14,15 +14,15 @@ app = dash.Dash(__name__)
 fig1 = px.scatter(advertising_data, 
                  x="TV", y="Sales", size='Newspaper',
                  color='Radio', trendline="ols",
-                 title='OLS Regression Results')
-
+                 title='Another way to see effects of several predictors on response:')
 # Create the image plot 
-img_url = 'pic.png'
-img = io.imread(img_url)
-fig2 = px.imshow(img)        # work on hiding axes and hover 
+# img_url = 'pic.png'
+# img = io.imread(img_url)
+# fig2 = px.imshow(img)    
 
-app.layout = html.Div([dcc.Graph(id='scatter-plot',figure=fig1),
-                       dcc.Graph(id='image-plot',figure=fig2)])
+# app.layout = html.Div([dcc.Graph(id='scatter-plot',figure=fig1),
+#                        dcc.Graph(id='image-plot',figure=fig2)])
+app.layout = html.Div([dcc.Graph(id='scatter-plot',figure=fig1)])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
